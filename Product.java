a04f9ee02b8b227c85379cd2782d6611e56362cc
zhangt2333 <zhangt2333@gmail.com>
2020-03-12 23:33:06 +0800

    Try to commit with a previous time.

--- Product.java
+++ Product.java
@@ -2,5 +2,6 @@ public class Product {
     public static void main(String[] args) {
         System.out.println("I'm version 10.0!");
         System.out.println("A bad feature!");
+        // nothing
     }
 } 
